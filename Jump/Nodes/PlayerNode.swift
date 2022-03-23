016865606079e99a5656cc91f1e767080c73f6ea
maemi <dbsrl759@naver.com>
2022-03-23 20:22:53 +0900

    Jump_Spritekit_tutorial

--- Jump/Nodes/PlayerNode.swift
+++ Jump/Nodes/PlayerNode.swift
@@ -9,4 +9,40 @@ import SpriteKit
 
 class PlayerNode: SKNode {
     
+    //MARK: - Properties
+    private var player: SKShapeNode!
+    private var diff = 0
+    private let radius: CGFloat = 30.0
+    
+    //MARK: - Initializes
+    init(diff: Int) {
+        super.init()
+        self.diff = diff
+        
+        self.name = "Player"
+        self.zPosition = 10.0
+        
+        self.setupPhysics()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+//MARK: - Setups
+extension PlayerNode {
+    
+    private func setupPhysics() {
+        player = SKShapeNode(circleOfRadius: radius)
+        player.name = "Player"
+        player.zPosition = .pi
+        player.fillColor = .red
+        player.physicsBody = SKPhysicsBody(circleOfRadius: radius * 0.8)
+        player.physicsBody?.isDynamic = false
+        player.physicsBody?.linearDamping = 0.0
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.affectedByGravity = true
+        player.physicsBody?.usesPreciseCollisionDetection = true
+    }
 }
